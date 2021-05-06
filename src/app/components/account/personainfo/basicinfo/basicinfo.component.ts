cf4068cd57a2b6765e1a64dee707e759eeeae51f
Sabri <sabrigharbi99@gmail.com>
2021-05-06 09:19:23 +0100

    small edits

--- src/app/components/account/personainfo/basicinfo/basicinfo.component.ts
+++ src/app/components/account/personainfo/basicinfo/basicinfo.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicinfo',
   templateUrl: './basicinfo.component.html',
-  styleUrls: ['./basicinfo.component.css']
+  styleUrls: ['./basicinfo.component.css'],
 })
 export class BasicinfoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSave(form: NgForm) {
+    console.log(form.value);
   }
-
 }
