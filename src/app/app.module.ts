9db9aa10dfe937829b471de15073d98f9a7b3c85
Sabri <sabrigharbi99@gmail.com>
2021-05-10 05:00:58 +0100

    clean UP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { ProfileOverviewComponent } from './components/profile-overview/profile-overview.component';
 import { ArticleOverviewComponent } from './components/article-overview/article-overview.component';
-import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ArticleComponent } from './components/article/article.component';
-import { AuthorOverviewComponent } from './components/author-overview/author-overview.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { ReplyComponent } from './components/reply/reply.component';
-import { RelatedComponent } from './components/related/related.component';
-import { ImgComponent } from './components/img/img.component';
-import { ContentComponent } from './components/content/content.component';
-import { BadgeComponent } from './components/badge/badge.component';
-import { ShareComponent } from './components/share/share.component';
 import { WriterComponent } from './components/writer/writer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AccountComponent } from './components/account/account.component';
@@ -67,19 +60,12 @@ import { NewpostComponent } from './components/newpost/newpost.component';
     HeaderComponent,
     ProfileOverviewComponent,
     ArticleOverviewComponent,
-    NavigationBarComponent,
     FooterComponent,
     ProfileComponent,
     ArticleComponent,
-    AuthorOverviewComponent,
     QuoteComponent,
     CommentComponent,
     ReplyComponent,
-    RelatedComponent,
-    ImgComponent,
-    ContentComponent,
-    BadgeComponent,
-    ShareComponent,
     WriterComponent,
     HomeComponent,
     AuthComponent,
