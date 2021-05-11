68eddc2e7aa06f9c0ce7dc1234d4d3681c56e83d
Mohamed Yassine Elkhadiri <mohamedyassine.elkhadiri@insat.u-carthage.tn>
2021-05-11 03:02:43 +0100

    Modified the new post

--- src/app/components/newpost/newpost.component.ts
+++ src/app/components/newpost/newpost.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { User } from 'src/app/auth/user.model';
+import { Article } from 'src/app/model/article.model';
+import { ArticlesService } from 'src/app/services/articles.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-newpost',
@@ -7,10 +11,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./newpost.component.css'],
 })
 export class NewpostComponent implements OnInit {
-  constructor() {}
+   title : string;
+   resume : string;
+   content : string;
+   pathToImg : string;
+
+  constructor(
+    private articleService : ArticlesService,
+    private userService : UsersService,
+  ) {}
 
   ngOnInit(): void {}
 
   onFileChanged(event) {}
-  onSave(form: NgForm) {}
+  onSave(form: NgForm) {
+    let newArticle = new Article(
+    "",
+    this.title,
+    this.resume,
+    this.pathToImg,
+    this.userService.getLoggedUser().fname,
+    this.userService.getLoggedUser().photo,
+    this.content,
+    "123");
+    this.articleService.addArticle(newArticle);
+  }
 }
